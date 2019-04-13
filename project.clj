950c58121b7de1d10b784b76f8cf3008a46de8d0
Jonathan Harford <jonathanh@mayvenn.com>
2019-04-13 07:04:47 -0700

    Update morse

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [environ "1.1.0"]
-                 [morse "0.4.0"]
+                 [morse "0.4.1"]
                  [postgresql "9.3-1102.jdbc41"]
                  [org.clojure/java.jdbc "0.7.9"]]
   :main ^:skip-aot epyc.core
