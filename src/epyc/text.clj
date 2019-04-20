ccc6c62426f2a3080edd5b0ccda9f3f5be3dadec
Jonathan Harford <jonathanh@mayvenn.com>
2019-04-20 10:26:45 -0700

    Update text

--- src/epyc/text.clj
+++ src/epyc/text.clj
@@ -1,15 +1,16 @@
 (ns epyc.text
   (:require [clojure.string :as str]))
 
-(def start #_"I administer games of Eat Poop You Cat. Type `/play` to play, or `/help` for more information."
-  "starting!")
+(def start "I administer games of Eat Poop You Cat. Type `/play` to play, or `/help` for more information.")
 
 (def help #_"[Eat Poop You Cat](http://boardgamegeek.com/boardgame/30618/eat-poop-you-cat)"
   "helping!") ; TODO improve much
 
 (def already-playing "You're already playing a turn!")
-(def first-turn "Gimme first turn text!")
-(def turn-done "Thanks for turn")
-(def request-text "Gimme text")
-(def request-photo "Gimme photo")
-(def confused "I'm confused.")
+(def first-turn "You'll be starting off a new game. Please give me a sentence describing a scene. Be as detailed and strange as you like!")
+(def turn-done "Thanks for playing! I'll message you when the game is done!")
+(def request-text "Please describe the following picture. _Infer details! Decode a narrative!_")
+(def request-photo "Please draw/sculpt/3d-craft the following scenario, and send me the picture. _No words! Light your picture well!_")
+(def confused "I'm not sure what to do with that, since you're not in a game right now. Maybe try `/help`?")
+(def game-done-1 "The game is finished! Here are all the turns:")
+(def game-done-2 "Thanks for playing!")
