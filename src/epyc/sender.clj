d3c7ef2daf6b44a526084f3044c4d16294839f2e
Jonathan Harford <jonathanh@mayvenn.com>
2019-05-24 16:42:34 -0700

    Clarify: "to-id" vs ambiguous "id"

--- src/epyc/sender.clj
+++ src/epyc/sender.clj
@@ -3,14 +3,14 @@
    [morse.api :as t]))
 
 (defprotocol ISender
-  (send-text [this id text])
+  (send-text [this to-id text])
   (forward-message [this to-id from-id message-id]))
 
 (defrecord Sender
     [token]
   ISender
-  (send-text [{token :token} id text]
-    (t/send-text token id {:parse_mode "Markdown"} text))
+  (send-text [{token :token} to-id text]
+    (t/send-text token to-id {:parse_mode "Markdown"} text))
 
   (forward-message [{token :token} to-id from-id message-id]
     (t/forward-message token to-id from-id message-id)))
