6f1e85e878bfba966fb1bb9f7dcf96819d718d38
Jonathan Harford <jonathanh@mayvenn.com>
2019-05-24 17:26:17 -0700

    Add a min-lein-verson

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Administers games of Eat Poop You Cat over Telegram"
   :url "https://github.com/JonathanHarford/epyc-email"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [environ "1.1.0"]
                  [morse "0.4.2"]
